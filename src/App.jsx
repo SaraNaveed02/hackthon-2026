b64f9571bbb7713963edb4b05f302383ae59c24b
SaraNaveed02 <saranaveed307@gmail.com>
2026-02-15 15:07:25 +0500

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
-// App.jsx mein BrowserRouter wali line ko aise update karein:
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Signin from './auth/login';
+import AdminDashboard from './admin/AdminDashboard';
+import UserDashboard from './components/UserDashboard';
+import Hero from './components/HeroSection';
+import MyReport from './components/MyReport';
+// import MyComplaint  from './components/MyComplaints';
+import './App.css';
 
-<BrowserRouter basename="/hackthon"> 
+function App() {
+  return (
+   <BrowserRouter basename="/hackthon"> 
   <Routes>
     <Route path="/" element={<Hero />} />
     <Route path="/login" element={<Signin />} />
@@ -9,3 +18,7 @@
     <Route path="/admindashboard" element={<AdminDashboard />} />
   </Routes>
 </BrowserRouter>
+  );
+}
+
+export default App;
